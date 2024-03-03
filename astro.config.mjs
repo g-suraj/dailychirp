c56614685c8a8386d91191fea501ccd8c2298686
g-suraj <contactgsuraj@gmail.com>
2024-03-03 21:08:20 +0000

    Test subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,9 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   outDir: "docs",
-  site: "https://suraj.fyi/dailychirp",
-  base: "/dailychirp",
-  devToolbar: { enabled: false },
-  vite: {
-    base: "https://suraj.fyi/dailychirp",
-  },
   build: {
     assets: "assets",
   },
+  site: "https://suraj.fyi/dailychirp",
+  devToolbar: { enabled: false },
 });
